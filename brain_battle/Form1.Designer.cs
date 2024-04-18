03f6accb262fa4809c29051c42f18408559ce5a7
allgorilla <pow.9093wgtm@docomo.ne.jp>
2024-04-19 07:11:26 +0900

    PixtureBoxを表示

--- brain_battle/Form1.Designer.cs
+++ brain_battle/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace brain_battle
+﻿using System.Windows.Forms;
+
+namespace brain_battle
 {
     partial class Form1
     {
@@ -6,6 +8,9 @@
         ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private PictureBox[] PbxP1 = new PictureBox[9];
+        private PictureBox[] PbxP2 = new PictureBox[9];
+        private PictureBox[] PbxBoard = new PictureBox[9];
 
         /// <summary>
         ///  Clean up any resources being used.
@@ -28,10 +33,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            // 
+            // Form1
+            // 
+            SuspendLayout();
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            for (int i = 0; i < 9; i++) {
+                SetPictureBox(PbxBoard, 210, i);
+                SetPictureBox(PbxP1, 0, i);
+                SetPictureBox(PbxP2, 420, i);
+            }
+
+            ResumeLayout(false);
+        }
+
+        private void SetPictureBox(PictureBox[] pbx, int j, int i)
+        {
+            pbx[i] = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pbx[i]).BeginInit();
+ 
+            pbx[i].BackgroundImageLayout = ImageLayout.Center;
+            pbx[i].Image = Resource1.tile;
+            pbx[i].Location = new Point( 50+j+65*(i%3), 50+65*(i/3) );
+            pbx[i].Name = "pictureBox1";
+            pbx[i].Size = new Size(64, 64);
+            pbx[i].TabIndex = 0;
+            pbx[i].TabStop = false;
+            pbx[i].Click += pictureBox1_Click;
+
+            Controls.Add(pbx[i]);
+            ((System.ComponentModel.ISupportInitialize)pbx[i]).EndInit();
         }
 
         #endregion
--- brain_battle/Form1.cs
+++ brain_battle/Form1.cs
@@ -6,5 +6,10 @@ namespace brain_battle
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
